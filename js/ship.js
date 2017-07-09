f9a8fc34fe572865c4a38b0653d54d393fa22458
nlazarova <nelly_l05@dir.bg>
2017-07-09 11:10:36 +0300

    v17

--- js/ship.js
+++ js/ship.js
@@ -20,28 +20,28 @@ $(document).ready(function(){
         $("#cloud").fadeIn(3000);
         $("#cloud").animate({left: '+850px'}, 100000, function(){});
 
-        $("#first").hide(1000);//скриване на всичко от div с id=first
+        $("#first").hide(1000);//Hide everything from div with id=first
         $("#second").fadeIn(3000);
         $("#myTable").fadeIn(3000);
-        $("td").addClass("water");//на всички <td> се присвоява клас water
+        $("td").addClass("water");//All <td> is assigned a class of 'water'
        
-        //======присвояване клас ship на случайно избрани клетки======//
-       // var shipArr=[];
+        //======Assigning a ship class to randomly selected cells======//
+      
         for (var numShip = 0; numShip < 3; numShip++) {
                 
         var col = Math.floor((Math.random() * 5) + 1);
-       // document.getElementById("demo").innerHTML = col;
+       
         var row = Math.floor((Math.random() * 6) + 0);
-       // document.getElementById("demo1").innerHTML = row;
-        var idtd = col*10 + row; 
+       
+        var idtd = col*10 + row;
+
         var residtd = idtd.toString();
+
         if (document.getElementById(residtd).className != "ship") {
-            //document.getElementById("anew").className = "ship";
             document.getElementById(residtd).className = "ship";
             console.log(idtd);
-            shipArr[numShip]= idtd; 
-          //  console.log(shipArr[numShip]);
-            //numShip=numShip+1;            
+            shipArr[numShip]= idtd;          
+                       
         } else if (col!=6) {
             col=col+1;
             } else {
@@ -49,76 +49,62 @@ $(document).ready(function(){
             }
             var idtd = col*10 + row; 
             var residtd = idtd.toString();
-            document.getElementById(residtd).className = "ship";
-           // console.log(idtd);
-            shipArr[numShip]= idtd;
-           // console.log(shipArr[numShip]); 
-
-           // numShip=numShip+1; 
-        }
-   
-       // $( ".ship" ).hide();
-    //document.getElementById("45").style()
-        //$("#anew").addClass("ship");
-        //document.getElementById("demo3").innerHTML = idTd;
+            document.getElementById(residtd).className = "ship";           
+            shipArr[numShip]= idtd;          
+           
+        }  
+       
     
 
 
-//========== сменя цвета на клетката от синьо в сиво при избор на тази клетка, ако не е кораб, ако е кораб се показва той
+//========== Changes the cell color when selecting this cell if it is not a ship showing 'X' if a ship is shown
     var numberHitShip=0;
     var notHit=0;
     $("td").click(function(){ 
         
-       // $("#text1").text("Браво! Ти позна всички кораби!");
+       
        if ($(this).hasClass("ship") ) {
            $(this).css("background", "url('img/ship.png') no-repeat center center" );
            document.getElementById("notship").innerHTML = "Браво! Ти позна кораб!";
            numberHitShip = numberHitShip + 1;
-           //alert(numberHitShip);
+           
            if (numberHitShip>=3) {
-            //какво става когато са открити всички кораби
-           // alert(numberHitShip);
+            //What happens when all the ships are found
+          
             var notHit1 = notHit + 3;
             document.getElementById("notship").innerHTML = "Браво! Ти позна всички кораби след "+notHit1 +" опита!";
            
            }
-          // $(this).show();    
+             
         }else if (numberHitShip<3) {
             var color = $(this).css('background-color');
-            if (color != '#0ba2e3') {
-           // if (($(this).css("background-color"))!= "#cccccc") {
+            if (color != '#0ba2e3') {           
                 notHit = notHit+1;
              }   else {
                 notHit = notHit;
              }
-            //============} else { //за 
-           //$(this).css("background-color", '#cccccc');
-           //смяна на сивия '#cccccc' със син цвят '#0ba2e3'
+            //============
+           
+           //Changing the background and text color of the selected non-ship cell
            $(this).css({"background-color": '#0ba2e3', "color": "red"});
            var thisHotHitId=$(this).attr('id');
            var resRowNot = thisHotHitId.substr(0, 1);
            var resColNot = thisHotHitId.substr(1, 1);
-          // alert(resColNot);
-          // alert(thisHotHitId, resRowNot, resColNot);
-          // alert($(this).attr('id'));
-
-                //notHit = notHit+1;
-           //да се изпише колко кораби има в тази редица и в тази колона
+                          
+           //To show how many ships there are in this row and this column
              
           var residtdArr0 = shipArr[0].toString();
           var residtdArr1 = shipArr[1].toString();
           var residtdArr2 = shipArr[2].toString();
            document.getElementById("notship").innerHTML = "Не уцели!";
-           // console.log(shipArr[0]);
-           //console.log(residtdArr1);
+           
            var resRowShips0 = residtdArr0.substr(0, 1);//
            var resColShips0 = residtdArr0.substr(1, 1);//
            var resRowShips1 = residtdArr1.substr(0, 1);//
            var resColShips1 = residtdArr1.substr(1, 1);//
            var resRowShips2 = residtdArr2.substr(0, 1);//
            var resColShips2 = residtdArr2.substr(1, 1);//
-            //console.log("row ",resRowShips0);
-             //console.log("col ",resColShips0);
+            
              if ((resRowNot==resRowShips0) || (resRowNot==resRowShips1) || (resRowNot==resRowShips2)) {
                  document.getElementById("notship").innerHTML = "Не уцели! На този ред има кораб.";
                  if ((resRowNot==resRowShips0) && (resRowNot==resRowShips1)) {document.getElementById("notship").innerHTML = "Не уцели! На този ред има поне два кораба.";}
@@ -132,17 +118,15 @@ $(document).ready(function(){
                  if ((resColNot==resColShips2) && (resColNot==resColShips1)) {document.getElementById("notship").innerHTML = "Не уцели! На тази колона има поне два кораба.";}
              }
 
-         //=======}//else за сивия цвят, да не брой повторно натискане върху такава клетка
+         
         }
 
 
-    });//===End======= Край на функцията, която сменя цвета на клетката от синьо в сиво при избор на тази клетка, ако не е кораб, ако е кораб се показва той
-
+    });//===End======= The end of the function that changes the color of the text and the background into a cell that is selected and is not a ship, if it is a ship it displays
 
 });  //End "#start" click(function()
      
-    $("#refresh").click(function(){
-        //jQuery("body").load(window.location.href);
+    $("#refresh").click(function(){        
         location.reload();
     });  //End "#refresh" click(function()
      
